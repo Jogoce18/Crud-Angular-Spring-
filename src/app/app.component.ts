f12039717f1f4114642e697d8b0b478405241bdc
wesleysilva25 <vetwesley@hotmail.com>
2024-03-18 19:55:03 -0300

    Feat: Added toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [MatToolbarModule, RouterOutlet]
 })
 export class AppComponent {
   title = 'crud-angular';
