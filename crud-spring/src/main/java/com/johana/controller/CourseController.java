d042782ddbd94ef3dd5416c5ad878bdabdc34cf1
Johana <gomezjohana655@gmail.com>
2024-03-24 15:11:01 -0300

    feat:Update spring e Angular

--- crud-spring/src/main/java/com/johana/controller/CourseController.java
+++ crud-spring/src/main/java/com/johana/controller/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List; // Importe a classe List correta
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.johana.model.Course; // Importe a classe Course correta
@@ -21,7 +22,7 @@ public class CourseController {
     }
 
     @GetMapping
-    public List<Course> list() {
+    public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
     }
 }
--- crud-spring/src/main/java/com/johana/model/Course.java
+++ crud-spring/src/main/java/com/johana/model/Course.java
@@ -6,6 +6,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
@@ -13,6 +15,7 @@ import lombok.Data;
 public class Course {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
+  @JsonProperty("_id")
   private Long id;
 
   @Column(length = 200, nullable = false)
