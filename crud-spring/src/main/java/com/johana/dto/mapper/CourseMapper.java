e0d5466a64adbf1a1d16ed2d8fcae226b3767beb
Johana <gomezjohana655@gmail.com>
2024-03-24 16:54:31 -0300

    feat: adicionei dto

---
+++ crud-spring/src/main/java/com/johana/dto/mapper/CourseMapper.java
@@ -0,0 +1,66 @@
+package com.johana.dto.mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import com.johana.dto.CourseDTO;
+import com.johana.dto.LessonDTO;
+import com.johana.enums.Category;
+import com.johana.model.Course;
+import com.johana.model.Lesson;
+
+@Component
+public class CourseMapper {
+
+    public CourseDTO toDTO(Course course) {
+        if (course == null) {
+            return null;
+        }
+        List<LessonDTO> lessons = course.getLessons()
+                .stream()
+                .map(lesson -> new LessonDTO(lesson.getId(), lesson.getName(),
+                        lesson.getYoutubeUrl()))
+                .collect(Collectors.toList());
+        return new CourseDTO(course.getId(), course.getName(), course.getCategory().getValue(),
+                lessons);
+    }
+
+    public Course toEntity(CourseDTO courseDTO) {
+
+        if (courseDTO == null) {
+            return null;
+        }
+
+        Course course = new Course();
+        if (courseDTO.id() != null) {
+            course.setId(courseDTO.id());
+        }
+        course.setName(courseDTO.name());
+        course.setCategory(convertCategoryValue(courseDTO.category()));
+
+        List<Lesson> lessons = courseDTO.lessons().stream().map(lessonDTO -> {
+            var lesson = new Lesson();
+            lesson.setId(lessonDTO.id());
+            lesson.setName(lessonDTO.name());
+            lesson.setYoutubeUrl(lessonDTO.youtubeUrl());
+            lesson.setCourse(course);
+            return lesson;
+        }).collect(Collectors.toList());
+        course.setLessons(lessons);
+
+        return course;
+    }
+
+    public Category convertCategoryValue(String value) {
+        if (value == null) {
+            return null;
+        }
+        return switch (value) {
+            case "Front-end" -> Category.FRONT_END;
+            case "Back-end" -> Category.BACK_END;
+            default -> throw new IllegalArgumentException("Categoria inválida: " + value);
+        };
+    }
+}
