0d02d73604abe20b09f729c4daffac016cccbcb8
Johana <gomezjohana655@gmail.com>
2024-03-24 16:32:34 -0300

    feat: adicionei controller , repository e service

--- crud-spring/src/main/java/com/johana/dto/CoursePageDTO.java
+++
@@ -1,7 +0,0 @@
-package com.johana.dto;
-
-import java.util.List;
-
-public record CoursePageDTO(List<CourseDTO> courses, long totalElements, int totalPages) {
-
-}
--- crud-spring/src/main/java/com/johana/exception/RecordNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.johana.exception;
-
-public class RecordNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public RecordNotFoundException(Long id) {
-        super("Registro não encontrado com o id: " + id);
-    }
-}
